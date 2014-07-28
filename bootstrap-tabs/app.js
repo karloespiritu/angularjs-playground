40912b47929e5d47f30fc7a0bd63552678cd01a1
Karlo Espiritu <karloespiritu@gmail.com>
2014-07-28 11:27:43 +0800

    added test for bootstrap tabs

---
+++ bootstrap-tabs/app.js
@@ -0,0 +1,15 @@
+var app = angular.module('app', ['ui.bootstrap']);
+
+app.controller('MainController', function($scope) {
+
+  $scope.tabs = [
+    { title:'Dynamic Title 1', content:'Dynamic content 1' },
+    { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
+  ];
+
+  $scope.alertMe = function() {
+    setTimeout(function() {
+      alert('You\'ve selected the alert tab!');
+    });
+  };
+});
